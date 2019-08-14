2664c51f01d377c42450d430e8c77634e53276b6
David Martin <demartins37@yahoo.com>
2019-08-14 16:28:12 -0400

    deploying to heroku setting up JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
-  var connection = process.env.JAWSDB_URL
+  var connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   var connection = mysql.createConnection({
     host: "localhost",
